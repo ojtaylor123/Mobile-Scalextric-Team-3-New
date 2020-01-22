d6ede8fce343288a5f0b29109afc103f110d89cf
nathanjbutler <56346120+nathanjbutler@users.noreply.github.com>
2020-01-22 15:32:38 +0000

    Update singleplayer.js

--- scripts/states/singleplayer/singleplayer.js
+++ scripts/states/singleplayer/singleplayer.js
@@ -1,1 +1,18 @@
+(function () {
+
+    'use strict';
+
+    var app = angular.module('singleplayer.loseState', []);
+
+    app.config(function ($stateProvider) {
+        $stateProvider.state('singleplayer', {
+            url: '/singleplayer',
+            templateUrl: 'scripts/states/singleplayer/singleplayer.html',
+            controller: 'singleplayerCtrl as vm',
+            cache: false
+        })
+    });
+
+})();
+
 
