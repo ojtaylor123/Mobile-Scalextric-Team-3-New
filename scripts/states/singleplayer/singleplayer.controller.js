51fc9d8586494b25c04776bca2085513304fe97a
nathanjbutler <56346120+nathanjbutler@users.noreply.github.com>
2020-01-22 15:32:35 +0000

    Update singleplayer.controller.js

--- scripts/states/singleplayer/singleplayer.controller.js
+++ scripts/states/singleplayer/singleplayer.controller.js
@@ -1,1 +1,18 @@
+(function () {
+
+    'use strict';
+
+    var app = angular.module('app.singleplayerState');
+
+    app.controller('singleplayerCtrl', winCtrl);
+
+    singleplayerCtrl.$inject = [];
+
+    function singleplayerCtrl() {
+        var vm = angular.extend(this, {});
+        return vm;
+    }
+
+})();
+
 
