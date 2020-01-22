dde7da91f5e354474700e8633e20a7a571b2cf24
nathanjbutler <56346120+nathanjbutler@users.noreply.github.com>
2020-01-22 15:28:28 +0000

    Update lose.module.js

--- scripts/states/lose/lose.module.js
+++ scripts/states/lose/lose.module.js
@@ -1,1 +1,17 @@
+(function () {
+
+    'use strict';
+
+    var app = angular.module('app.loseState', []);
+
+    app.config(function ($stateProvider) {
+        $stateProvider.state('lose', {
+            url: '/lose',
+            templateUrl: 'scripts/states/lose/lose.html',
+            controller: 'loseCtrl as vm',
+            cache: false
+        })
+    });
+
+})();
 
