caf917a6397e653dfb1a55fd2a46d05c394b30b9
Spamford <56346213+Spamford@users.noreply.github.com>
2020-01-22 15:55:13 +0000

    Update app.js
    To be updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
 var app = angular.module("myApp", []);
+
+$stateProvider.state(onboardingState);
+$stateProvider.state(queueState);
+$stateProvider.state(raceState);
+$stateProvider.state(casualState);
+$stateProvider.state(singleplayerState);
+$stateProvider.state(winState);
+$stateProvider.state(loseState);
