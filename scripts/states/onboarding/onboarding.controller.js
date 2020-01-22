d33e847d78c53e6dc992a6b91be7b1f076a7a144
ojtaylor123 <47565091+ojtaylor123@users.noreply.github.com>
2020-01-22 15:19:08 +0000

    Update onboarding.controller.js

--- scripts/states/onboarding/onboarding.controller.js
+++ scripts/states/onboarding/onboarding.controller.js
@@ -1,1 +1,16 @@
+(function () {
 
+    'use strict';
+
+    var app = angular.module('app.onboardingState');
+
+    app.controller('onboardingCtrl', onboardingCtrl);
+
+    aboutCtrl.$inject = [];
+
+    function onboardingCtrl() {
+        var vm = angular.extend(this, {});
+        return vm;
+    }
+
+})();
