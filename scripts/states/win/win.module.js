7c46dd646a964bb27dc1dad1a05ebde2fc047a8c
nathanjbutler <56346120+nathanjbutler@users.noreply.github.com>
2020-01-22 15:28:34 +0000

    Update win.module.js

--- scripts/states/win/win.module.js
+++ scripts/states/win/win.module.js
@@ -1,1 +1,16 @@
+(function () {
 
+    'use strict';
+
+    var app = angular.module('app.winState', []);
+
+    app.config(function ($stateProvider) {
+        $stateProvider.state('win', {
+            url: '/win',
+            templateUrl: 'scripts/states/win/win.html',
+            controller: 'winCtrl as vm',
+            cache: false
+        })
+    });
+
+})();
