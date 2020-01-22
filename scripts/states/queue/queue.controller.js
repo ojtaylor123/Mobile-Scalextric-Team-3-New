83b794d4c8dab98b24b728ee0369bac74d312c8c
Spamford <56346213+Spamford@users.noreply.github.com>
2020-01-22 15:33:39 +0000

    Update queue.controller.js

--- scripts/states/queue/queue.controller.js
+++ scripts/states/queue/queue.controller.js
@@ -1,1 +1,16 @@
+(function () {
 
+    'use strict';
+
+    var app = angular.module('app.queueState');
+
+    app.controller('queueCtrl', queueCtrl);
+
+    queueCtrl.$inject = [];
+
+    function queueCtrl() {
+        var vm = angular.extend(this, {});
+        return vm;
+    }
+
+})();
