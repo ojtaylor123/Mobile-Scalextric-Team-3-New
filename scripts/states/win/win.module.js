4e35322352e430565f5763b91db9507303bfc25d
nathanjbutler <56346120+nathanjbutler@users.noreply.github.com>
2020-01-24 14:54:58 +0000

    Update win.module.js

--- scripts/states/win/win.module.js
+++ scripts/states/win/win.module.js
@@ -6,7 +6,7 @@
 
     app.config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider.state('win', {
-            url: 'win',
+            url: '/win',
             templateUrl: 'scripts/states/win/win.html',
             controller: 'winCtrl as vm',
             cache: false
