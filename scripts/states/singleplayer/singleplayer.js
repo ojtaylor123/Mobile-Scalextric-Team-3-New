901dc9b549da7670bf1c484942f0fe28668b5fda
Spamford <56346213+Spamford@users.noreply.github.com>
2020-01-24 15:47:31 +0000

    Update singleplayer.js

--- scripts/states/singleplayer/singleplayer.js
+++ scripts/states/singleplayer/singleplayer.js
@@ -1,18 +1,19 @@
 (function () {
-
     'use strict';
 
-    var app = angular.module('app.singleplayerState', ['ui.router']);
-
-    app.config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('singleplayer', {
-            url: '/singleplayer',
-            templateUrl: 'scripts/states/singleplayer/singleplayer.html',
-            controller: 'singleplayerCtrl as vm',
-            cache: false
-        })
-        $urlRouterProvider.otherwise('/onboarding');
-    });
-
+    angular
+        .module('app.singleplayerState', [
+            'ui.router'
+        ])
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('singleplayer', {
+                    cache: false,
+                    url: '/singleplayer',
+                    templateUrl: 'scripts/states/singleplayer/singleplayer.html',
+                    controller: 'singleplayerCtrl as vm'
+                })
+            $urlRouterProvider.otherwise('/onboarding');
+        });
 })();
 
