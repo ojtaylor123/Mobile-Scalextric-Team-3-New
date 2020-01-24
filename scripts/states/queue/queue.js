d9877291e1a536a8a563362f38b2a430a5b54297
Spamford <56346213+Spamford@users.noreply.github.com>
2020-01-24 14:12:23 +0000

    Update queue.js

--- scripts/states/queue/queue.js
+++ scripts/states/queue/queue.js
@@ -2,15 +2,16 @@
 
     'use strict';
 
-    var app = angular.module('app.queueState', []);
+    var app = angular.module('app.queueState', [ui.router]);
 
-    app.config(function ($stateProvider) {
+    app.config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider.state('queue', {
             url: '/queue',
             templateUrl: 'scripts/states/about/about.html',
             controller: 'queueCtrl as vm',
             cache: false
         })
+        $urlRouterProvider.otherwise('/onboarding');
     });
 
 })();
