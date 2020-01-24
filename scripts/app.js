e74e06723fb38889277da04c5ec7167bfcef4ffd
James Sweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-01-24 14:41:51 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@
     $rootScope.$on('$stateChangeError', function (toState, toParams, fromState, fromParams){
       console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
     
-      $state.get('race').error = { code: 123, descriptions: 'Exception stack trace'}
+      $state.get('onboarding').error = { code: 123, descriptions: 'Exception stack trace'}
       return $state.go('onboarding');
     });
   })
