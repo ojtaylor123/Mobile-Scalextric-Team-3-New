c7a2fbed51514dce148fa74e65ae0dfe7b88fc50
Spamford <56346213+Spamford@users.noreply.github.com>
2020-01-24 14:05:38 +0000

    Update casual.js

--- scripts/states/casual/casual.js
+++ scripts/states/casual/casual.js
@@ -2,15 +2,16 @@
 
     'use strict';
 
-    var app = angular.module('app.casualState', []);
+    var app = angular.module('app.casualState', ['ui.router']);
 
-    app.config(function ($stateProvider) {
+    app.config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider.state('casual', {
             url: '/casual',
             templateUrl: 'scripts/states/casual/casual.html',
             controller: 'casualCtrl as vm',
             cache: false
-        });
+        })
+        $urlRouterProvider.otherwise('/onboarding');
     });
 
 })();
