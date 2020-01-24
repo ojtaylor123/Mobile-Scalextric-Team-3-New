8f098193ab694dc184a039a500279f583d119555
James Sweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-01-24 14:56:33 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,17 @@
 (function () {
-  var app = angular.module("myApp", ['ui.router']);
-  
-  app.run(function ($state, $rootScope) {
-    $rootScope.$on('$stateChangeError', function (toState, toParams, fromState, fromParams){
-      console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
-    
-      $state.get('onboarding').error = { code: 123, descriptions: 'Exception stack trace'}
-      return $state.go('onboarding');
-    });
-  })
+  angular.module('myApp', [
+    'ui.router'
+  ])
+
+    .run(function ($state, $rootScope) {
+      $rootScope.$on('$stateChangeError', function (toState, toParams, fromState, fromParams) {
+        console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
+
+        $state.get('onboarding').error = { code: 123, descriptions: 'Exception stack trace' }
+        return $state.go('onboarding');
+      });
+
+    })
 })();
 
 
