1cf4b0ae29dfdc5000e2c6f1fbf5a5c713c13696
nathanjbutler <56346120+nathanjbutler@users.noreply.github.com>
2020-01-24 15:21:14 +0000

    Update win.module.js

--- scripts/states/win/win.module.js
+++ scripts/states/win/win.module.js
@@ -1,18 +1,38 @@
-(function () {
+// (function () {
 
+//     'use strict';
+
+//     var app = angular.module('app.winState', ['ui.router']);
+
+//     app.config(function ($stateProvider, $urlRouterProvider) {
+//         $stateProvider
+//             .state('win', {
+//                 url: '/win',
+//                 templateUrl: 'scripts/states/win/win.html',
+//                 controller: 'winCtrl as vm',
+//                 cache: false
+//             })
+//         $urlRouterProvider.otherwise('casual');
+//     });
+
+// })();
+
+(function () {
     'use strict';
 
-    var app = angular.module('app.winState', ['ui.router']);
-
-    app.config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('win', {
-                url: '/win',
-                templateUrl: 'scripts/states/win/win.html',
-                controller: 'winCtrl as vm',
-                cache: false
-            })
-        $urlRouterProvider.otherwise('casual');
-    });
-
+    angular
+        .module('winState', [
+            'ui.router',
+            'ngAnimate'
+        ])
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('win', {
+                    cache: false,
+                    url: '/win',
+                    templateUrl: 'scripts/states/win/win.html',
+                    controller: 'winCtrl as vm'
+                })
+            $urlRouterProvider.otherwise('/casual');
+        });
 })();
