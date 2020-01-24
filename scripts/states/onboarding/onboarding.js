3cc6ca1c8bba978e2d756845a002a13f51ec1e4a
James Sweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-01-24 15:08:35 +0000

    Update onboarding.js

--- scripts/states/onboarding/onboarding.js
+++ scripts/states/onboarding/onboarding.js
@@ -1,17 +1,18 @@
 (function () {
-
     'use strict';
 
-    var app = angular.module('app.onboardingState', ['ui.router]);
-
-    app.config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('onboarding', {
-            url: '/onboarding',
-            templateUrl: 'scripts/states/onboarding/onboarding.html',
-            controller: 'onboardingCtrl as vm',
-            cache: false
-        })
-        $urlRouterProvider.otherwise('/onboarding');                                             
-    });
-
+    angular
+        .module('app.onboardingState', [
+            'ui.router'
+        ])
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('onboarding', {
+                    cache: false,
+                    url: '/onboarding',
+                    templateUrl: 'scripts/states/onboarding/onboarding.html',
+                    controller: 'onboardingCtrl as vm'
+                })
+            $urlRouterProvider.otherwise('/onboarding');
+        });
 })();
