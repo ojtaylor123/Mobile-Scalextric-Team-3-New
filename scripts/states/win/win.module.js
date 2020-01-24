2bd2c3fe4e1479ccedc766b1073dc963c8871978
Spamford <56346213+Spamford@users.noreply.github.com>
2020-01-24 14:22:28 +0000

    Update win.module.js

--- scripts/states/win/win.module.js
+++ scripts/states/win/win.module.js
@@ -2,15 +2,16 @@
 
     'use strict';
 
-    var app = angular.module('app.winState', []);
+    var app = angular.module('app.winState', ['ui.router']);
 
-    app.config(function ($stateProvider) {
+    app.config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider.state('win', {
             url: '/win',
             templateUrl: 'scripts/states/win/win.html',
             controller: 'winCtrl as vm',
             cache: false
         })
+        $urlRouterProvider.otherwise('/casual');
     });
 
 })();
