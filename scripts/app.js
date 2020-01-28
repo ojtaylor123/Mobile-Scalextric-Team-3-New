a03fa1b9e681ac7c3769f89772a1993c22b25a69
James Sweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-01-28 14:08:29 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -7,35 +7,43 @@ angular.module('myApp').config(['$stateProvider','$urlRouterProvider', function(
     $stateProvider
         .state('onboarding', {
             url: '/onboarding',
-            templateUrl : 'scripts/states/onboarding/onboarding.html'
+            templateUrl : 'scripts/states/onboarding/onboarding.html',
+            cache: false
     })
         .state('queue', {
             url: '/queue',
-            templateUrl : 'scripts/states/queue/queue.html'
+            templateUrl : 'scripts/states/queue/queue.html',
+            cache: false
     })
         .state('race', {
             url: '/race',
-            templateUrl : 'scripts/states/race/race.html'
+            templateUrl : 'scripts/states/race/race.html',
+            cache: false
     })
         .state('casual', {
             url: '/casual',
-            templateUrl : 'scripts/states/casual/casual.html'
+            templateUrl : 'scripts/states/casual/casual.html',
+            cache: false
     })
         .state('singleplayer', {
             url: '/singleplayer',
-            templateUrl : 'scripts/states/singleplayer/singleplayer.html'
+            templateUrl : 'scripts/states/singleplayer/singleplayer.html',
+            cache: false
     })
         .state('challenge', {
             url: '/challenge',
-            templateUrl : 'scripts/states/challenge/challenge.html'
+            templateUrl : 'scripts/states/challenge/challenge.html',
+            cache: false
     })
         .state('win', {
             url: '/win',
-            templateUrl : 'scripts/states/win/win.html'
+            templateUrl : 'scripts/states/win/win.html',
+            cache: false
     })
         .state('lose', {
             url: '/lose',
-            templateUrl : 'scripts/states/lose/lose.html'
+            templateUrl : 'scripts/states/lose/lose.html',
+            cache: false
     })
 }]);
 
