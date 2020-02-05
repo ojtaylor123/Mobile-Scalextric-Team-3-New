f03928d2140be69797b295545b2720df14dd7b88
James Sweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-02-05 14:58:34 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ui.router']);
+/*angular.module('myApp', ['ui.router']);
 
 angular.module('myApp').config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
     // sets default state
@@ -50,4 +50,95 @@ angular.module('myApp').config(['$stateProvider','$urlRouterProvider', function(
 angular.module("myApp").run(function ($rootScope, $state, $stateParams) {
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
-});
+});*/
+
+angular.module('myApp', ['ui.router']);
+
+
+angular.module('myApp').config(config);
+
+config.$inject = [
+    '$stateProvider',
+    '$urlRouterProvider'
+]
+
+function config($stateProvider, $urlRouterProvider) {
+    
+    var onboardingState = {
+        name: 'onboarding',
+        url: '/onboarding?uuid&brokerHost&brokerPort&username&password&ssl',
+        params: {
+            uuid : {
+                dynamic: false
+            },
+            brokerHost: {
+                dynamic: false
+            },
+            brokerPort: {
+                dynamic: false
+            },
+            username: {
+                dynamic : false
+            },
+            password: {
+                dynamic : false
+            },
+            ssl:{
+                dynamic : false
+            }
+        },
+        templateUrl: 'scripts/states/onboarding/onboarding.html',
+        controller: 'onboardingCtrl',
+        controllerAs: 'onboarding',
+        resolve: {
+            broker: ['$stateParams','brokerDetails', function($stateParams, brokerDetails) {
+                if($stateParams.uuid) brokerDetails.UUID = $stateParams.uuid;
+                if($stateParams.brokerHost) brokerDetails.HOST = $stateParams.brokerHost;
+                if($stateParams.brokerPort) brokerDetails.PORT = $stateParams.brokerPort;
+                if($stateParams.username) brokerDetails.USERNAME = $stateParams.username;
+                if($stateParams.password) brokerDetails.PASSWORD = $stateParams.password;
+                if($stateParams.ssl) brokerDetails.SSL = ($stateParams.ssl.toLowerCase() == 'true');                
+            }]
+        }
+    }   
+
+    var casualState = {
+        name: 'casual',
+        url: '/casual',
+        templateUrl: 'scripts/states/casual/casual.html',
+        controller: 'casualCtrl',
+        controllerAs: 'casual',
+        params: {
+            channel: null,
+            ip_address: null
+        },
+        //resolve used to check if transition contains channel and ipaddress params
+        resolve: {
+            parameters: ['$q', '$state','$stateParams', function ($q, $state,$stateParams) {
+                var deferred = $q.defer();
+               
+                if ($stateParams.channel === null) {
+                    $state.transitionTo('onboarding', {});
+                }else{
+                    deferred.resolve();
+                }
+
+                return deferred.promise;
+            }]
+        }
+    };
+
+    $stateProvider.state(onboardingState);
+    $stateProvider.state(casualState);
+
+    $urlRouterProvider.otherwise('/onbording');
+}
+
+angular.module('app').run(run);
+run.$inject = [
+]
+
+function run() {
+    console.log('version 1.0.0 Mobile Scalextric 3');
+}
+
