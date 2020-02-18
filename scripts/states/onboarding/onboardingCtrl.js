cf3720a07d576a5fe074da9519f7b46b148c319e
James Sweetland <56346421+JamesSweetland@users.noreply.github.com>
2020-02-18 15:42:08 +0000

    Update onboardingCtrl.js

--- scripts/states/onboarding/onboardingCtrl.js
+++ scripts/states/onboarding/onboardingCtrl.js
@@ -1,18 +1,56 @@
-(function () {
-    'use strict';
+angular.module('app').contoller('onboardingCtrl', onboardingCtrl);
 
-    angular
-        .module('app.onboardingState', [
-            'ui.router'
-        ])
-        .config(function ($stateProvider, $urlRouterProvider) {
-            $stateProvider
-                .state('onboarding', {
-                    cache: false,
-                    url: '/onboarding',
-                    templateUrl: 'scripts/states/onboarding/onboarding.html',
-                    controller: 'onboardingCtrl as vm'
-                })
-            $urlRouterProvider.otherwise('/onboarding');
-        });
-})();
+onboardingCtrl.$inject = [
+    '$rootScope',
+    '$state',
+    'mqttService',
+    'brokerDetails'
+];
+
+function onboardingCtrl($rootScope, $state, mqttService, brokerDetails) {
+    var vm = this;
+    
+    //creates channel array and number of channels
+    vm.channels = Array.apply(null, {
+        length: 5
+    }).map(Function.call, Number)
+    vm.channel = 0; //default channel
+    
+    console.log(brokerDetails);
+    
+    vm.go = go;
+    
+    function go(valid) {
+        if (!valid) {
+            alert("Invalid Details")
+        }
+        else {
+            mqttService.initialize(brokerDetails.HOST, brokerDetails.PORT);
+            mqttService.onConnectionLost(function (){
+                console.error("connection lost");
+            });
+            
+            var mqttOptions = {};
+            
+            if(brokerDetails.SSL) { mqttOptions.useSSL = brokerDetails.SSL; }
+            if(brokerDetails.USERNAME) {
+                mqttOptions.userName = brokerDetails.USERNAME
+                if(brokerDetails.PASSWORD){
+                    mqttOptions.password = brokerDetails.PASSWORD;
+                }
+            }
+            
+            mqttService.connect(function (success, error) {
+                if(success){
+                    $state.transitionTo('casual', {
+                        channel: vm.channel,
+                    })
+                }
+                else if (error) {
+                    console.log(error)
+                    alert(`Could Not Connect to ${brokerDetails.HOST}:${brokerDetails.PORT}`)
+                }
+            },mqttOptions)
+        }
+    }
+}
