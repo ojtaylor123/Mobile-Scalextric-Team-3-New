cdea2de03774b0a13ebccba13e35cfd113d4402e
nathanjbutler <56346120+nathanjbutler@users.noreply.github.com>
2020-02-21 12:21:48 +0000

    Update win.controller.js

--- scripts/states/win/win.controller.js
+++ scripts/states/win/win.controller.js
@@ -14,3 +14,40 @@
     }
 
 })();
+function go(valid) {
+    if (!valid) {
+        alert("Invalid Details")
+    } else {
+        mqttService.initialize(brokerDetails.HOST, brokerDetails.PORT);
+        mqttService.onConnectionLost(function () {
+            console.error("connection lost");
+        });
+
+
+        var mqttOptions = {};
+
+        if (brokerDetails.SSL) { mqttOptions.useSSL = brokerDetails.SSL; }
+        if (brokerDetails.USERNAME) {
+            mqttOptions.userName = brokerDetails.USERNAME;
+            if(brokerDetails.PASSWORD){
+                mqttOptions.password = brokerDetails.PASSWORD;
+            }
+        }
+
+        
+
+        mqttService.connect(function (success, error) {
+            if (success) {
+                $state.transitionTo('casual',
+                    {
+                        channel: vm.channel,
+                    });
+            } else if (error) {
+                console.log(error)
+                alert(`Could Not Connect to ${brokerDetails.HOST}:${brokerDetails.PORT}`)
+            }
+
+        },mqttOptions)
+    }
+
+}
