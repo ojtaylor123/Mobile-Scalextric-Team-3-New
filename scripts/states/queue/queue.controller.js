b42a84422df5293a35998e28efbaed7dfb4a89e2
Spamford <56346213+Spamford@users.noreply.github.com>
2020-02-25 16:06:27 +0000

    Update queue.controller.js

--- scripts/states/queue/queue.controller.js
+++ scripts/states/queue/queue.controller.js
@@ -12,24 +12,6 @@
 
         var vm = angular.extend(this, {});
 
-        function weaponBox() {
-
-
-            var myArray = [
-            "Oil Slick",
-            "Smart Bomb",
-            "Puncture"
-            ];
-        
-            var randomWeapon = myArray[Math.floor(Math.random()*myArray.length)]; 
-            
-            document.getElementById('weapon').innerHTML = randomWeapon;
-        
-        }
-
-        setInterval(weaponBox, 5000)
-
-
         return vm;
     }
 
