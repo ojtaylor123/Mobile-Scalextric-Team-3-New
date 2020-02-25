d0f63d47ad5ac701ee1a997909642f5a14a87d97
Spamford <56346213+Spamford@users.noreply.github.com>
2020-02-25 16:07:02 +0000

    Update race.controller.js

--- scripts/states/race/race.controller.js
+++ scripts/states/race/race.controller.js
@@ -12,4 +12,21 @@
         var vm = angular.extend(this, {});
         return vm;
     }
+    
+    function weaponBox() {
+
+
+            var myArray = [
+            "Oil Slick",
+            "Smart Bomb",
+            "Puncture"
+            ];
+        
+            var randomWeapon = myArray[Math.floor(Math.random()*myArray.length)]; 
+            
+            document.getElementById('weapon').innerHTML = randomWeapon;
+        
+        }
+
+        setInterval(weaponBox, 5000)
 })();
